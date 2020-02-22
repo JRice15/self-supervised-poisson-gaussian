3019bc048b0aa1ce7c7d03c88f22aeac2d204652
Julian Rice <julianrice@dotcomjungle.com>
2020-02-22 10:12:02 -0800

    resnet semi integrated

--- nets.py
+++ nets.py
@@ -1,7 +1,10 @@
 import numpy as np
-from keras import Input
+from keras import Input, utils
 from keras.models import Model
-from keras.layers import Input, Lambda, Conv2D, LeakyReLU, UpSampling2D, MaxPooling2D, ZeroPadding2D, Cropping2D, Concatenate, Reshape, GlobalAveragePooling2D, BatchNormalization, Add, Subtract
+from keras.layers import (Input, Lambda, Conv2D, LeakyReLU, UpSampling2D, 
+                        MaxPooling2D, ZeroPadding2D, Cropping2D, Concatenate, 
+                        Reshape, GlobalAveragePooling2D, BatchNormalization, 
+                        Add, Subtract, add, Activation, GlobalMaxPooling2D)
 from keras.initializers import Constant
 import keras.backend as K
 import tensorflow as tf
@@ -165,6 +168,239 @@ def _vshifted_pool(x):
 
   return x
 
+
+
+def identity_block(input_tensor, kernel_size, filters, stage, block):
+    """The identity block is the block that has no conv layer at shortcut.
+    # Arguments
+        input_tensor: input tensor
+        kernel_size: default 3, the kernel size of
+            middle conv layer at main path
+        filters: list of integers, the filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+    # Returns
+        Output tensor for the block.
+    """
+    filters1, filters2, filters3 = filters
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    # x = Conv2D(filters1, (1, 1),
+    #                   kernel_initializer='he_normal',
+    #                   name=conv_name_base + '2a')(input_tensor)
+    x = _vshifted_conv(input_tensor, filters1, conv_name_base + '2a')
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    # x = Conv2D(filters2, kernel_size,
+    #                   padding='same',
+    #                   kernel_initializer='he_normal',
+    #                   name=conv_name_base + '2b')(x)
+    x = _vshifted_conv(x, filters2, conv_name_base + '2b')
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    # x = Conv2D(filters3, (1, 1),
+    #                   kernel_initializer='he_normal',
+    #                   name=conv_name_base + '2c')(x)
+    x = _vshifted_conv(x, filters3, conv_name_base + '2c')
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    x = add([x, input_tensor])
+    x = Activation('relu')(x)
+    return x
+
+
+def conv_block(input_tensor,
+               kernel_size,
+               filters,
+               stage,
+               block,
+               strides=(2, 2)):
+    """A block that has a conv layer at shortcut.
+    # Arguments
+        input_tensor: input tensor
+        kernel_size: default 3, the kernel size of
+            middle conv layer at main path
+        filters: list of integers, the filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+        strides: Strides for the first conv layer in the block.
+    # Returns
+        Output tensor for the block.
+    Note that from stage 3,
+    the first conv layer at main path is with strides=(2, 2)
+    And the shortcut should have strides=(2, 2) as well
+    """
+    filters1, filters2, filters3 = filters
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    # x = Conv2D(filters1, (1, 1), strides=strides,
+    #                   kernel_initializer='he_normal',
+    #                   name=conv_name_base + '2a')(input_tensor)
+    x = _vshifted_conv(input_tensor, filters1, conv_name_base + '2a')
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    # x = Conv2D(filters2, kernel_size, padding='same',
+    #                   kernel_initializer='he_normal',
+    #                   name=conv_name_base + '2b')(x)
+    x = _vshifted_conv(x, filters2, conv_name_base + '2b')
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    # x = Conv2D(filters3, (1, 1),
+    #                   kernel_initializer='he_normal',
+    #                   name=conv_name_base + '2c')(x)
+    x = _vshifted_conv(x, filters3, conv_name_base + '2c')
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    # shortcut = Conv2D(filters3, (1, 1), strides=strides,
+    #                          kernel_initializer='he_normal',
+    #                          name=conv_name_base + '1')(input_tensor)
+    shortcut = _vshifted_conv(input_tensor, filters3, conv_name_base + '1')
+    shortcut = BatchNormalization(
+        axis=bn_axis, name=bn_name_base + '1')(shortcut)
+
+    x = add([x, shortcut])
+    x = Activation('relu')(x)
+    return x
+
+
+def ResNet50(input_tensor,
+             input_shape,
+             weights=None,
+             pooling=None):
+    """Instantiates the ResNet50 architecture.
+    Note that the data format convention used by the model is
+    the one specified in your Keras config at `~/.keras/keras.json`.
+    # Arguments
+        include_top: whether to include the fully-connected
+            layer at the top of the network.
+        weights: one of `None` (random initialization),
+              'imagenet' (pre-training on ImageNet),
+              or the path to the weights file to be loaded.
+        input_tensor: optional Keras tensor (i.e. output of `layers.Input()`)
+            to use as image input for the model.
+        input_shape: optional shape tuple, only to be specified
+            if `include_top` is False (otherwise the input shape
+            has to be `(224, 224, 3)` (with `channels_last` data format)
+            or `(3, 224, 224)` (with `channels_first` data format).
+            It should have exactly 3 inputs channels,
+            and width and height should be no smaller than 32.
+            E.g. `(200, 200, 3)` would be one valid value.
+        pooling: Optional pooling mode for feature extraction.
+            - `None` means that the output of the model will be
+                the 4D tensor output of the
+                last convolutional block.
+            - `avg` means that global average pooling
+                will be applied to the output of the
+                last convolutional block, and thus
+                the output of the model will be a 2D tensor.
+            - `max` means that global max pooling will
+                be applied.
+    # Returns
+        A Keras model instance.
+    # Raises
+        ValueError: in case of invalid argument for `weights`,
+            or invalid input shape.
+    """
+
+    if not (weights is None or os.path.exists(weights)):
+        raise ValueError('The `weights` argument should be either '
+                         '`None` (random initialization), '
+                         'or the path to the weights file to be loaded.')
+
+    if input_tensor is None:
+        img_input = Input(shape=input_shape)
+    else:
+        if not K.is_keras_tensor(input_tensor):
+            img_input = Input(tensor=input_tensor, shape=input_shape)
+        else:
+            img_input = input_tensor
+
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+
+    print("1", img_input)
+
+    x = ZeroPadding2D(padding=(3, 3), name='conv1_pad')(img_input)
+    # x = Conv2D(64, (7, 7),
+    #                   strides=(2, 2),
+    #                   padding='valid',
+    #                   kernel_initializer='he_normal',
+    #                   name='conv1')(x)
+    x = _vshifted_conv(x, 48, 'conv1')
+
+    x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+    x = Activation('relu')(x)
+    # x = ZeroPadding2D(padding=(1, 1), name='pool1_pad')(x)
+    # x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+
+    print("2", x)
+
+    x = conv_block(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1))
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='b')
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='c')
+
+    print("3", x)
+
+    x = conv_block(x, 3, [128, 128, 512], stage=3, block='a')
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='b')
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='c')
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='d')
+
+    print("4", x)
+
+    x = conv_block(x, 3, [256, 256, 1024], stage=4, block='a')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='c')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='d')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='e')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='f')
+
+    print("5", x)
+
+    x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
+
+    print("6", x)
+
+    if pooling is not None:
+        if pooling == 'avg':
+            x = GlobalAveragePooling2D()(x)
+        elif pooling == 'max':
+            x = GlobalMaxPooling2D()(x)
+
+    # Ensure that the model takes into account
+    # any potential predecessors of `input_tensor`.
+    inputs = utils.get_source_inputs(input_tensor)
+    
+    # Create model
+    model = Model(inputs, x, name='resnet50')
+
+    # Load weights
+    if weights is not None:
+        model.load_weights(weights)
+
+    return model
+
+
+
+
 def _vertical_blindspot_network(x):
   """ Blind-spot network; adapted from noise2noise GitHub
     Each row of output only sees input pixels above that row
@@ -172,26 +408,37 @@ def _vertical_blindspot_network(x):
   skips = [x]
 
   n = x
+  print("0", n)
   n = _vshifted_conv(n, 48, 'enc_conv0')
   n = _vshifted_conv(n, 48, 'enc_conv1')
   n = _vshifted_pool(n)
   skips.append(n)
 
+  print("1", n)
+
   n = _vshifted_conv(n, 48, 'enc_conv2')
   n = _vshifted_pool(n)
   skips.append(n)
+
+  print("2", n)
 
   n = _vshifted_conv(n, 48, 'enc_conv3')
   n = _vshifted_pool(n)
   skips.append(n)
 
+  print("3", n)
+
   n = _vshifted_conv(n, 48, 'enc_conv4')
   n = _vshifted_pool(n)
   skips.append(n)
 
+  print("4", n)
+
   n = _vshifted_conv(n, 48, 'enc_conv5')
   n = _vshifted_pool(n)
   n = _vshifted_conv(n, 48, 'enc_conv6')
+
+  print("5", n)
 
   #-----------------------------------------------
   n = UpSampling2D(2)(n)
@@ -199,25 +446,35 @@ def _vertical_blindspot_network(x):
   n = _vshifted_conv(n, 96, 'dec_conv5')
   n = _vshifted_conv(n, 96, 'dec_conv5b')
 
+  print("6", n)
+
   n = UpSampling2D(2)(n)
   n = Concatenate(axis=3)([n, skips.pop()])
   n = _vshifted_conv(n, 96, 'dec_conv4')
   n = _vshifted_conv(n, 96, 'dec_conv4b')
+
+  print("7", n)
 
   n = UpSampling2D(2)(n)
   n = Concatenate(axis=3)([n, skips.pop()])
   n = _vshifted_conv(n, 96, 'dec_conv3')
   n = _vshifted_conv(n, 96, 'dec_conv3b')
 
+  print("8", n)
+
   n = UpSampling2D(2)(n)
   n = Concatenate(axis=3)([n, skips.pop()])
   n = _vshifted_conv(n, 96, 'dec_conv2')
   n = _vshifted_conv(n, 96, 'dec_conv2b')
 
+  print("9", n)
+
   n = UpSampling2D(2)(n)
   n = Concatenate(axis=3)([n, skips.pop()])
   n = _vshifted_conv(n, 96, 'dec_conv1a')
   n = _vshifted_conv(n, 96, 'dec_conv1b')
+
+  print("10", n)
 
   # final pad and crop for blind spot
   n = ZeroPadding2D([[1,0],[0,0]])(n)
@@ -226,40 +483,41 @@ def _vertical_blindspot_network(x):
   return n
 
 def blindspot_network(inputs):
-  b,h,w,c = K.int_shape(inputs)
-  #if h != w:
-    #raise ValueError('input shape must be square')
-  if h % 32 != 0 or w % 32 != 0:
-    raise ValueError('input shape (%d x %d) must be divisible by 32'%(h,w))
+    b,h,w,c = K.int_shape(inputs)
+    #if h != w:
+        #raise ValueError('input shape must be square')
+    if h % 32 != 0 or w % 32 != 0:
+        raise ValueError('input shape (%d x %d) must be divisible by 32'%(h,w))
 
-  # make vertical blindspot network
-  vert_input = Input([h,w,c])
-  vert_output = _vertical_blindspot_network(vert_input)
-  vert_model = Model(inputs=vert_input,outputs=vert_output)
+    # make vertical blindspot network
+    vert_input = Input([h,w,c])
+    # vert_output = _vertical_blindspot_network(vert_input)
+    # vert_model = Model(inputs=vert_input,outputs=vert_output)
+    vert_model = ResNet50(input_tensor=vert_input, input_shape=[h,w,c])
 
-  # run vertical blindspot network on rotated inputs
-  stacks = []
-  for i in range(4):
-      rotated = Lambda(lambda x: tf.image.rot90(x,i))(inputs)
-      if i == 0 or i == 2:
-          rotated = Reshape([h,w,c])(rotated)
-      else:
-          rotated = Reshape([w,h,c])(rotated)
-      out = vert_model(rotated)
-      out = Lambda(lambda x:tf.image.rot90(x,4-i))(out)
-      stacks.append(out)
+    # run vertical blindspot network on rotated inputs
+    stacks = []
+    for i in range(4):
+        rotated = Lambda(lambda x: tf.image.rot90(x,i))(inputs)
+        if i == 0 or i == 2:
+            rotated = Reshape([h,w,c])(rotated)
+        else:
+            rotated = Reshape([w,h,c])(rotated)
+        out = vert_model(rotated)
+        out = Lambda(lambda x:tf.image.rot90(x,4-i))(out)
+        stacks.append(out)
 
-  # concatenate outputs
-  x = Concatenate(axis=3)(stacks)
+    # concatenate outputs
+    x = Concatenate(axis=3)(stacks)
 
-  # final 1x1 convolutional layers
-  x = Conv2D(384, 1, kernel_initializer='he_normal', name='conv1x1_1')(x)
-  x = LeakyReLU(0.1)(x)
+    # final 1x1 convolutional layers
+    x = Conv2D(384, 1, kernel_initializer='he_normal', name='conv1x1_1')(x)
+    x = LeakyReLU(0.1)(x)
 
-  x = Conv2D(96, 1, kernel_initializer='he_normal', name='conv1x1_2')(x)
-  x = LeakyReLU(0.1)(x)
-  
-  return x
+    x = Conv2D(96, 1, kernel_initializer='he_normal', name='conv1x1_2')(x)
+    x = LeakyReLU(0.1)(x)
+    
+    return x
 
 def gaussian_blindspot_network(input_shape,mode,reg_weight=0):
     """ Create a variant of the Gaussian blindspot newtork.
