295b0c91c9e0d49c02b0fb225f2234fc37b3416b
Julian Rice <jrice15@calpoly.edu>
2020-05-26 22:50:07 -0700

    dropout 40 blocks

--- nets.py
+++ nets.py
@@ -483,7 +483,7 @@ def vshift_conv_2(x, channels_out, name, kernel_size=3, strides=1, bias=True):
 
 
 
-def resnet_v2(inputs, num_blocks=20, num_channels=48):
+def resnet_v2(inputs, num_blocks=40, num_channels=48):
 
     x = vshift_conv_2(inputs, num_channels, name="initial")
     x = LeakyReLU(0.2, name="relu-initial")(x)
